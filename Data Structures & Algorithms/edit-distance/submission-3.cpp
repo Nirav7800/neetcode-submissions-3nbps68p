9891929cd7e20d1bc802e4d61d0bfa1ce2921bb8
Nirav Patel <54497832+Nirav7800@users.noreply.github.com>
2026-05-01 11:25:43 +0530

    Add: edit-distance - submission-3

---
+++ Data Structures & Algorithms/edit-distance/submission-3.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int dp[101][101];
+    int solve(string word1,string word2,int n1,int n2) {
+        if(n2==0 || n1==0) {
+            return max(n1,n2);
+        }
+
+        if(dp[n1][n2]!=-1) return dp[n1][n2];
+        if(word1[n1-1]==word2[n2-1]) {
+            return solve(word1,word2,n1-1,n2-1);
+        }
+
+        //insert
+        int ic = 1+solve(word1,word2,n1,n2-1);
+
+        //delete
+        int dc = 1+solve(word1,word2,n1-1,n2);
+
+        int rc = 1+solve(word1,word2,n1-1,n2-1);
+
+        return dp[n1][n2]=min(ic,min(dc,rc));
+    } 
+    int minDistance(string word1, string word2) {
+        int n1 = word1.size();
+        int n2 = word2.size();
+        // for(int i=0;i<=n1;i++) {
+        //     for(int j=0;j<=n2;j++) {
+        //         dp[i][j]=-1;
+        //     }
+        // }
+        // return solve(word1,word2,n1,n2);
+        for(int i=0;i<=n1;i++) {
+            for(int j=0;j<=n2;j++) {
+                if(i==0 || j==0) {
+                    dp[i][j]=max(i,j);
+                    continue;
+                } 
+                if(word1[i-1]==word2[j-1]) dp[i][j]=dp[i-1][j-1];
+                else dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+            }
+        }
+        return dp[n1][n2];
+    }
+};
