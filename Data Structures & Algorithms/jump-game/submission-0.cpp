df6e61930356ddd8eb557fb478766f77d84b65d8
Nirav Patel <54497832+Nirav7800@users.noreply.github.com>
2026-05-03 11:52:47 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int max_reach = nums[0];
+        int n = nums.size();
+        for(int i=1;i<n;i++) {
+            if(max_reach<i) {
+                return false;
+            }
+            max_reach = max(max_reach,i+nums[i]);
+        }
+        return true;
+    }
+};
