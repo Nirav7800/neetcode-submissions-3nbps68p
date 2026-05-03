a86f05924e62ffa4d1dae480545aa5650de926cf
Nirav Patel <54497832+Nirav7800@users.noreply.github.com>
2026-05-03 11:29:49 +0530

    Add: task-scheduling - submission-3

---
+++ Data Structures & Algorithms/task-scheduling/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char,int>mp;
+        int max_freq=1;
+        for(auto t:tasks){
+            mp[t]++;
+            max_freq=max(mp[t],max_freq);
+        }
+        int count = 0;
+        for(auto i:mp) {
+            if(max_freq==i.second) {
+                count++;
+            }
+        }
+        int time = (max_freq-1)*(n+1)+(count);
+        return max((int)tasks.size(),time);
+    }
+};
